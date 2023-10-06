29ea18c75b9eaf19b999a626f0fa575b98b7070d
moshiur01 <mr.moshiur314@gmail.com>
2023-10-06 10:52:47 +0600

    auth login api configed wuth redux

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -14,6 +14,7 @@ export const axiosBaseQuery =
       meta?: IMeta;
       params?: AxiosRequestConfig["params"];
       headers?: AxiosRequestConfig["headers"];
+      contentType?: string;
     },
     unknown,
     unknown
--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,3 +1,5 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || " http://localhostL3030";
+  return (
+    process.env.NEXT_PUBLIC_API_BASE_URL || " http://localhostL3030/api/v1"
+  );
 };
