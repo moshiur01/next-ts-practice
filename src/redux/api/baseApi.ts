e5889e5721ee2890c2da0734297e6e8f8b5238db
moshiur01 <mr.moshiur314@gmail.com>
2023-10-06 10:32:11 +0600

    api baseQuery with axios done

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,12 @@
+// Need to use the React-specific entry point to allow generating React hooks
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { getBaseUrl } from "@/helpers/config/envConfig";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery({ baseUrl: getBaseUrl() }),
+  endpoints: (builder) => ({}),
+  tagTypes: ["user"],
+});
